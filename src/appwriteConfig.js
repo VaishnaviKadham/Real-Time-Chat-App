e90a290fd6a0932dbf05551944172f2d8f3103a3
VaishnaviKadham <118585128+VaishnaviKadham@users.noreply.github.com>
2024-03-29 20:29:40 +0530

    first commit

---
+++ src/appwriteConfig.js
@@ -0,0 +1,21 @@
+import { Client, Databases ,Account} from 'appwrite';
+
+
+export const API_ENDPOINT= import.meta.env.VITE_APPWRITE_URL
+export const PROJECT_ID =import.meta.env.VITE_APPWRITE_PROJECT_ID
+export const DATABASE_ID =import.meta.env.VITE_APPWRITE_DATABASE_ID
+export const COLLECTION_ID_MESSAGES =import.meta.env.VITE_APPWRITE_COLLECTION_ID
+
+
+const client = new Client();
+
+client
+    .setEndpoint(API_ENDPOINT)
+    .setProject(PROJECT_ID);
+
+
+    export const account = new Account(client);
+    export const databases = new Databases(client);
+
+
+export default client;
